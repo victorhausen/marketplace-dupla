dbcc30f26cc8136e5cd098f80a02f117c62649c2
Matheus <matheusblopes10@gmail.com>
2021-01-04 15:21:59 -0300

    Implementado backend

---
+++ backend/controller.py
@@ -0,0 +1,11 @@
+class Controller:
+
+    def __init__(self, path):
+        self.path = path
+    
+    def create(self, data):
+        self.arquivo = open(self.path, 'a')
+
+        self.arquivo.write(str(data)+"\n")
+
+        self.arquivo.close()
