6fa945152dedf2778d5f492cfa9874716b2a1cdf
Marcello Santana <marcellosantana@live.com>
2021-01-05 16:41:30 -0300

    Adição das funções get_market e get_product

--- backend/controller.py
+++ backend/controller.py
@@ -1,3 +1,4 @@
+import json
 class Controller:
 
     def __init__(self, path):
@@ -9,3 +10,31 @@ class Controller:
         self.arquivo.write(str(data)+"\n")
 
         self.arquivo.close()
+    def get_market(self):
+        lista_market = []
+        self.arquivo = open(self.path, 'r')
+        for a in self.arquivo:
+            json_market = json.loads(a.replace("'", '"'))
+            print(json_market['name'])
+            print(json_market['description'])
+            market = {
+                'name': json_market['name'],
+                'description': json_market['description']
+            }
+            lista_market.append(market)
+        return lista_market
+    
+        def get_product(self):
+        lista_product = []
+        self.arquivo = open(self.path, 'r')
+        for a in self.arquivo:
+            json_product = json.loads(a.replace("'", '"'))
+            print(json_product['name'])
+            print(json_product['description'])
+            market = {
+                'name': json_product['name'],
+                'description': json_product['description'],
+                'price': json_product['price']
+            }
+            lista_market.append(market)
+        return lista_market
