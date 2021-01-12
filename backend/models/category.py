a0cdb8b3d13f2e3d1b2c29fa8aaee72cf544dfd1
Ralph <ralph.souza@olist.com>
2021-01-12 18:02:38 +0000

    Abertura de diretórios pra models, criação de classes para produtos e categorias.

---
+++ backend/models/category.py
@@ -0,0 +1,5 @@
+class Category:
+    def __init__(self, name: str, description: str, id: int = None) -> None:
+        self.name = name
+        self.description = description
+        self.id = id
---
+++ backend/models/product.py
@@ -0,0 +1,6 @@
+class Product:
+    def __init__(self, name: str, description: str, price: float, id: int = None) -> None:
+        self.name = name
+        self.description = description
+        self.price = price
+        self.id = id
