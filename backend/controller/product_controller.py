50c6b5124accba0b472aa932cc9160dc1c2c28c5
Ralph <ralph.souza@olist.com>
2021-01-12 18:06:40 +0000

    Refatoração de código e estruturas de classe.

--- backend/controller/product_controller.py
+++ backend/controller/product_controller.py
@@ -1,8 +1,11 @@
-from ..dao_db.product_dao_db import create_product, get_products
+from backend.dao_db.product_dao_db import create_product, get_products
+from backend.models.product import Product
 
-def creating_product(name, description, price)-> None:
-        create_product(name, description, price)
+
+def creating_product(pr: Product) -> None:
+    create_product(pr)
+
 
 def list_products() -> list:
-        products = get_products()
-        return products
+    products = get_products()
+    return products
