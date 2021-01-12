350fa1046f222356b870fd7eb5966e73afd1663b
anapaula-nestor <anapaula.nestor@olist.com>
2021-01-12 19:22:24 -0300

    inclusão da pasta model

---
+++ backend/models/marketplace.py
@@ -0,0 +1,5 @@
+class Marketplace:
+    def __init__(self, name: str, description: str, id: int = None):
+        self.name = name
+        self.description = description
+        self.id = id
---
+++ backend/models/seller.py
@@ -0,0 +1,6 @@
+class Seller:
+    def __init__(self, name: str, phone: str, email: str, id: int = None):
+        self.name = name
+        self.phone = phone
+        self.email = email
+        self.id = id
