e760caacfa9c13290dcfa65bdd6afec080c3b6ec
Ralph <ralph.souza@olist.com>
2021-01-12 18:24:50 +0000

    Refatoração de código e estruturas de classe.

--- backend/controller/category_controller.py
+++ backend/controller/category_controller.py
@@ -1,10 +1,13 @@
-#from ..dao_txt.category_dao_txt import get_categories, create_category
-from ..dao_db.category_dao_db import get_categories, create_category
-from .log_controller import write_log, get_log
+# from ..dao_txt.category_dao_txt import get_categories, create_category
+from backend.dao_db.category_dao_db import get_categories, create_category
+from backend.controller.log_controller import write_log
+from backend.models.category import Category
 
-def creating_category(category) -> None:
-    create_category(category)
+
+def creating_category(ca: Category) -> None:
+    create_category(ca)
     write_log(action="create", type="category")
+
 
 def list_categories() -> list:
     categories = get_categories()
