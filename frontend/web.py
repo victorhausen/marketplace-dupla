491b412390086a47a2d065eacab02b16f85827eb
Ralph <ralph.souza@olist.com>
2021-01-12 18:35:10 +0000

    Refatoração de código e estruturas de classe.

--- frontend/web.py
+++ frontend/web.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.append('.')
 
 from flask import Flask, render_template, request
@@ -8,13 +9,17 @@ from backend.controller.product_controller import creating_product, list_product
 from backend.controller.seller_controller import list_sellers, creating_seller
 from backend.controller.log_controller import get_log
 from backend.controller.category_controller import list_categories, creating_category
+from backend.models.product import Product
+from backend.models.category import Category
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
+
 @app.route('/')
 def index():
     return render_template('base_template.html')
+
 
 @app.route('/marketplaces', methods=['GET', 'POST'])
 def marketplaces():
@@ -25,38 +30,42 @@ def marketplaces():
         redirect("/")
     return render_template('create_marketplace.html')
 
+
 @app.route('/list_marketplaces')
 def listar_marketplace():
     lista_marketplace = list_marketplaces()
-    return render_template('list_marketplace.html', listar = lista_marketplace)
+    return render_template('list_marketplace.html', listar=lista_marketplace)
+
 
 @app.route('/products', methods=['GET', 'POST'])
 def products():
     if request.method == 'POST':
-        name = request.form['name_input']
-        description = request.form['description_input']
-        price = request.form['price_input']
-        creating_product(name, description, price)
+        pr = Product(request.form['name_input'], request.form['description_input'], request.form['price_input'])
+        creating_product(pr)
         redirect("/")
     return render_template('create_product.html')
+
 
 @app.route('/list_products')
 def listar_produtos():
     lista_produtos = list_products()
-    return render_template('list_product.html', produtos = lista_produtos)
+    return render_template('list_product.html', produtos=lista_produtos)
+
 
 @app.route('/categories', methods=['GET', 'POST'])
 def cadastrar_categorias():
     if request.method == 'POST':
-        data = request.form
-        creating_category(data)
+        ca = Category(request.form['name'], request.form['description'])
+        creating_category(ca)
         redirect("/")
     return render_template('create_categories.html')
+
 
 @app.route('/list_categories')
 def listar_categories():
     lista_categorias = list_categories()
-    return render_template('list_categories.html', lista = lista_categorias)
+    return render_template('list_categories.html', lista=lista_categorias)
+
 
 @app.route('/sellers', methods=['GET', 'POST'])
 def sellers():
@@ -65,14 +74,17 @@ def sellers():
         creating_seller(data)
     return render_template('create_seller.html')
 
+
 @app.route('/list_sellers')
 def list_seller():
     sellers_list = list_sellers()
-    return render_template('list_seller.html', sellers = sellers_list)
+    return render_template('list_seller.html', sellers=sellers_list)
+
 
 @app.route('/list_log')
 def lista_log():
     log_list = get_log()
-    return render_template('list_log.html', log = log_list)
+    return render_template('list_log.html', log=log_list)
+
 
 app.run(debug=True)
