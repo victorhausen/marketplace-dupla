3201d167486a12a4ed2ce784a20f4149816586bb
anapaula-nestor <anapaula.nestor@olist.com>
2021-01-13 21:32:22 -0300

    inclusão de update e delete para seller e marketplace

--- backend/controller/marketplace_controller.py
+++ backend/controller/marketplace_controller.py
@@ -1,4 +1,5 @@
-from backend.dao_db.marketplace_dao_db import create_marketplace, read_marketplaces
+from backend.dao_db.marketplace_dao_db import create_marketplace, read_marketplaces, update_marketplace, \
+    delete_marketplace, read_marketplace_by_id
 from backend.models.marketplace import Marketplace
 from backend.models.log import Log, current_date
 from backend.controller.log_controller import creating_log
@@ -11,5 +12,21 @@ def creating_marketplace(mp: Marketplace) -> None:
 
 def reading_marketplaces() -> list:
     marketplaces = read_marketplaces()
-    creating_log(Log(current_date(), 'list', 'marketplace'))
+    creating_log(Log(current_date(), 'read', 'marketplace'))
     return marketplaces
+
+
+def updating_marketplace(mp: Marketplace) -> None:
+    update_marketplace(mp)
+    creating_log(Log(current_date(), 'update', 'marketplace'))
+
+
+def deleting_marketplace(id: int) -> None:
+    delete_marketplace(id)
+    creating_log(Log(current_date(), 'delete', 'marketplace'))
+
+
+def reading_marketplace_by_id(id: int) -> Marketplace:
+    marketplace = read_marketplace_by_id(id)
+    creating_log(Log(current_date(), 'read_by_id', 'marketplace'))
+    return marketplace
--- backend/dao_db/seller_dao_db.py
+++ backend/dao_db/seller_dao_db.py
@@ -5,7 +5,8 @@ from backend.models.seller import Seller
 def create_seller(seller: Seller) -> None:
     with db_connection() as db:
         cursor = db.cursor()
-        cursor.execute(f"INSERT INTO seller(name, email, phone) values('{seller.name}','{seller.email}','{seller.phone}');")
+        cursor.execute(
+            f"INSERT INTO seller(name, email, phone) values('{seller.name}','{seller.email}','{seller.phone}');")
         db.commit()
 
 
@@ -13,9 +14,43 @@ def read_sellers() -> list:
     sellers = []
     with db_connection() as db:
         cursor = db.cursor()
-        cursor.execute("SELECT name, email, phone, id FROM seller;")
+        cursor.execute("SELECT name, phone, email,  id FROM seller;")
         list_sellers = cursor.fetchall()
         for sel in list_sellers:
-            result = Seller(sel[0], sel[2], sel[1])
+            result = Seller(sel[0], sel[1], sel[2], sel[3])
             sellers.append(result)
-    return sellers;
+    return sellers
+
+
+def update_seller(seller: Seller) -> None:
+    with db_connection() as db:
+        cursor = db.cursor()
+        cursor.execute(
+            f"""
+                update seller 
+                set name='{seller.name}', email='{seller.email}', phone='{seller.phone}'
+                where id={seller.id};
+            """
+        )
+    db.commit()
+
+
+def delete_seller(id: int) -> None:
+    with db_connection() as db:
+        cursor = db.cursor()
+        cursor.execute(
+            f"""
+                delete from seller
+                where id={id};
+            """
+        )
+    db.commit()
+
+
+def read_seller_by_id(id: int) -> Seller:
+    with db_connection() as db:
+        cursor = db.cursor()
+        cursor.execute(f"SELECT name, phone, email, id FROM seller where id ={id}")
+        seller = cursor.fetchall()
+        sel = Seller(seller[0][0], seller[0][1], seller[0][2], seller[0][3])
+    return sel
