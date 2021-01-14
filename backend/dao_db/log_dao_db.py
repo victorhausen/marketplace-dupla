452a0832b273b991bff6e488bd28ad6bf80fc006
Ralph <ralph.souza@olist.com>
2021-01-14 18:07:58 +0000

    Alinhamento e refatoração de código.

--- backend/dao_db/log_dao_db.py
+++ backend/dao_db/log_dao_db.py
@@ -1,24 +1,29 @@
-from backend.dao_db.connection import db_connection
+from backend.dao_db.connection import Connection
 from backend.models.log import Log, format_date_to_print
 
 
 def create_log(log: Log) -> None:
-    with db_connection() as db:
-        cursor = db.cursor()
-        cursor.execute(f"INSERT INTO log (date, description) values('{log.date}', '{log.action} {log.type_}');")
-        db.commit()
+    with Connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute(f"""
+                        INSERT INTO log 
+                        (date, description) 
+                        values('{log.date}', '{log.action} {log.type_}');
+                        """)
+        conn.commit()
 
 
 def read_logs() -> list:
     lista_logs = []
-    with db_connection() as db:
-        cursor = db.cursor()
+    with Connection() as conn:
+        cursor = conn.cursor()
         cursor.execute("SELECT * FROM log")
         logs = cursor.fetchall()
         for log in logs:
             description = log[2].split(' ')
             result = Log(format_date_to_print(log[1]), description[0], description[1])
             lista_logs.append(result)
+
     return lista_logs
 
 
