d68dfc95e2cc7b14b84842d66737db058837535e
Ralph <ralph.souza@olist.com>
2021-01-14 18:08:45 +0000

    Alinhamento e refatoração de código.

--- backend/dao_db/product_dao_db.py
+++ backend/dao_db/product_dao_db.py
@@ -1,56 +1,60 @@
-from backend.dao_db.connection import db_connection
+from backend.dao_db.connection import Connection
 from backend.models.product import Product
 
 
-def create_product(pr: Product) -> None:
-    with db_connection() as db:
-        cursor = db.cursor()
-        cursor.execute(
-            f"INSERT INTO product (name, description, price) values('{pr.name}', '{pr.description}', {pr.price});")
-        db.commit()
+def create_product(product: Product) -> None:
+    with Connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute(f"""
+                        INSERT INTO product 
+                        (name, description, price) 
+                        values('{product.name}', '{product.description}', {product.price});
+                        """)
+        conn.commit()
 
 
 def read_products() -> list:
     lista_products = []
-    with db_connection() as db:
-        cursor = db.cursor()
+    with Connection() as conn:
+        cursor = conn.cursor()
         cursor.execute("SELECT * FROM product")
         products = cursor.fetchall()
         for product in products:
             result = Product(product[1], product[2], product[3], product[0])
             lista_products.append(result)
+
     return lista_products
 
 
-def update_product(pr: Product):
-    with db_connection() as db:
-        cursor = db.cursor()
+def update_product(product: Product):
+    with Connection() as conn:
+        cursor = conn.cursor()
         cursor.execute(f"""
-                            UPDATE product 
-                            SET name='{pr.name}', description='{pr.description}', price={pr.price} 
-                            WHERE id={pr.id};
+                        UPDATE product 
+                        SET name='{product.name}', description='{product.description}', price={product.price} 
+                        WHERE id={product.id};
                         """)
-        db.commit()
+        conn.commit()
 
 
 def delete_product(id: int):
-    with db_connection() as db:
-        cursor = db.cursor()
+    with Connection() as conn:
+        cursor = conn.cursor()
         cursor.execute(f"""
                         DELETE FROM product
                         WHERE id={id};
                         """)
-        db.commit()
+        conn.commit()
 
 
 def read_product_by_id(id: int) -> Product:
-    with db_connection() as db:
-        cursor = db.cursor()
+    with Connection() as conn:
+        cursor = conn.cursor()
         cursor.execute(f"SELECT * FROM product WHERE id={id};")
         products = cursor.fetchall()
 
-        pr = Product(products[0][1], products[0][2], products[0][3], products[0][0])
+        results = Product(products[0][1], products[0][2], products[0][3], products[0][0])
 
-    return pr
+    return results
 
 
