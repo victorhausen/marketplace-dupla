fbeb4f270cf3150569e5b65f8c22675de8cddba9
Ralph <ralph.souza@olist.com>
2021-01-14 18:06:23 +0000

    Criação da classe Connection em connection.py.

--- backend/dao_db/connection.py
+++ backend/dao_db/connection.py
@@ -1,12 +1,18 @@
 import psycopg2
 
 
-def db_connection():
-    host = 'pgsql08-farm15.uni5.net'
-    user = 'topskills3'
-    database = 'topskills3'
-    password = 'olist21'
+class Connection:
+    def __get_connection_string(self):
+        host = 'pgsql08-farm15.uni5.net'
+        user = 'topskills3'
+        database = 'topskills3'
+        password = 'olist21'
+        connection_string = f'host={host} user={user} dbname={database} password={password}'
+        return connection_string
 
-    connection_string = f'host={host} user={user} dbname={database} password={password}'
-    db = psycopg2.connect(connection_string)
-    return db
+    def __enter__(self):
+        self.__connection = psycopg2.connect(self.__get_connection_string())
+        return self.__connection
+
+    def __exit__(self, type, value, trace):
+        self.__connection.close()
