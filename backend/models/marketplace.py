24cbd33fec8978df669451e38ef975d9c07e65d4
anapaula-nestor <anapaula.nestor@olist.com>
2021-01-18 13:18:55 -0300

    incluindo type hint

--- backend/models/marketplace.py
+++ backend/models/marketplace.py
@@ -5,25 +5,25 @@ class Marketplace:
         self.__id = id
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self.__name
 
     @name.setter
-    def name(self, name):
+    def name(self, name: str) -> None:
         self.__name = name
 
     @property
-    def description(self):
+    def description(self) -> str:
         return self.__description
 
     @description.setter
-    def description(self, description):
+    def description(self, description: str) -> None:
         self.__description = description
 
     @property
-    def id(self):
+    def id(self) -> int:
         return self.__id
 
     @id.setter
-    def id(self, id):
+    def id(self, id: int) -> None:
         self.__id = id
--- backend/models/seller.py
+++ backend/models/seller.py
@@ -6,33 +6,33 @@ class Seller:
         self.id = id
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self.__name
 
     @name.setter
-    def name(self, name):
+    def name(self, name: str) -> None:
         self.__name = name
 
     @property
-    def phone(self):
+    def phone(self) -> str:
         return self.__phone
 
     @phone.setter
-    def phone(self, phone):
+    def phone(self, phone: str) -> None:
         self.__phone = phone
 
     @property
-    def email(self):
+    def email(self) -> str:
         return self.__email
 
     @email.setter
-    def email(self, email):
+    def email(self, email: str) -> None:
         self.__email = email
 
     @property
-    def id(self):
+    def id(self) -> int:
         return self.__id
 
     @id.setter
-    def id(self, id):
+    def id(self, id: int) -> None:
         self.__id = id
