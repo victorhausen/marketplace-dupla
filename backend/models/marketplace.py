b4ab107e717b617a73bae1b4ce9e4bcd918cb54e
anapaula-nestor <anapaula.nestor@olist.com>
2021-01-18 12:09:08 -0300

    refatorando classe seller e marketplace

--- backend/models/marketplace.py
+++ backend/models/marketplace.py
@@ -1,5 +1,29 @@
 class Marketplace:
     def __init__(self, name: str, description: str, id: int = None):
-        self.name = name
-        self.description = description
-        self.id = id
+        self.__name = name
+        self.__description = description
+        self.__id = id
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
+
+    @property
+    def description(self):
+        return self.__description
+
+    @description.setter
+    def description(self, description):
+        self.__description = description
+
+    @property
+    def id(self):
+        return self.__id
+
+    @id.setter
+    def id(self, id):
+        self.__id = id
--- backend/models/seller.py
+++ backend/models/seller.py
@@ -4,3 +4,35 @@ class Seller:
         self.phone = phone
         self.email = email
         self.id = id
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
+
+    @property
+    def phone(self):
+        return self.__phone
+
+    @phone.setter
+    def phone(self, phone):
+        self.__phone = phone
+
+    @property
+    def email(self):
+        return self.__email
+
+    @email.setter
+    def email(self, email):
+        self.__email = email
+
+    @property
+    def id(self):
+        return self.__id
+
+    @id.setter
+    def id(self, id):
+        self.__id = id
